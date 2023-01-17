f49828e81e624196f9b7985f744fd7019a3eb1a6
Diego <diegoyon@outlook.com>
2023-01-16 22:03:24 -0600

    implement instant search and results

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
+import '@hotwired/turbo-rails';
+import 'controllers';
+
+// code
+let searchBar = document.getElementById('search-bar');
+let searchButton = document.getElementById('search-button');
+// console.log(searchBar);
+searchBar.addEventListener('input', filterList);
+
+function filterList() {
+  const filter = searchBar.value.toLowerCase();
+  const listItems = document.querySelectorAll('.article-item');
+
+  listItems.forEach((item) => {
+    let text = item.textContent;
+    if (text.toLowerCase().includes(filter.toLowerCase())) {
+      item.style.display = '';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
