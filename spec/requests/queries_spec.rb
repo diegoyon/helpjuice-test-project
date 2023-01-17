78875cb10ba0158dc0d508c6319a2cd45d2619fc
Diego <diegoyon@outlook.com>
2023-01-17 15:29:08 -0600

    add requests tests

--- spec/requests/queries_spec.rb
+++ spec/requests/queries_spec.rb
@@ -1,6 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe "Queries", type: :request do
+
+  before :each do
+    @user = User.create(email: 'test@gmail.com', password: 123456)
+    sign_in @user
+  end
+
   describe "GET /index" do
     it "returns http success" do
       get "/queries/index"
