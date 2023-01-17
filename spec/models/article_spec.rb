33f87492b344ca27a7e72bbb30476055f14af42b
Diego <diegoyon@outlook.com>
2023-01-17 14:51:38 -0600

    add model tests

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Article.new(title: "Test title", content: "Test content", author: "Test author")
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a title' do
+      subject.title = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without content' do
+      subject.content = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an author' do
+      subject.author = nil
+      expect(subject).to_not be_valid
+    end
+
+  end
 end
--- spec/models/query_spec.rb
+++ spec/models/query_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
 
 RSpec.describe Query, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject do
+    Query.new(body: "Test query", user_id: 1)
+  end
+
+  context 'validations' do
+    it 'is valid with valid attributes' do
+      expect(subject).to be_valid
+    end
+
+    it 'is not valid without a body' do
+      subject.body = nil
+      expect(subject).to_not be_valid
+    end
+
+    it 'is not valid without an user id' do
+      subject.user_id = nil
+      expect(subject).to_not be_valid
+    end
+  end
 end
