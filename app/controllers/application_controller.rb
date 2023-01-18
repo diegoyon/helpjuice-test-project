d2acb65e81743b55fcfac11ae37993ee673706d5
Diego <diegoyon@outlook.com>
2023-01-17 19:50:53 -0600

    add styles to pagination

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
   # request authentication to use the app
   before_action :authenticate_user!
+
+  # require pagination for array
   require 'will_paginate/array'
 end
